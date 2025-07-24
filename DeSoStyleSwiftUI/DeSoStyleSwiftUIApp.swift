33f1f94fd534e93d7d549de570eba6fd5090eb23
Durvas Shende <durwas@ranium.in>
2025-07-24 17:19:39 +0530

    Initial commit

---
+++ DeSoStyleSwiftUI/DeSoStyleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeSoStyleSwiftUIApp.swift
+//  DeSoStyleSwiftUI
+//
+//  Created by Durvas Shende on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DeSoStyleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeSoStyleSwiftUITests/DeSoStyleSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  DeSoStyleSwiftUITests.swift
+//  DeSoStyleSwiftUITests
+//
+//  Created by Durvas Shende on 24/07/25.
+//
+
+import Testing
+@testable import DeSoStyleSwiftUI
+
+struct DeSoStyleSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
